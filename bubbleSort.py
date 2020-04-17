7acee3411cf488abd9d21535d9a1bfb029d89c68
shahad-mahmud <shahad9381@gmail.com>
2020-04-17 22:49:23 +0600

    optimized bubble sort

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,6 +1,6 @@
 def bubbleSort(array):
     length = len(array)
-    for i in range(0, length):
+    for i in range(0, length - 1):
         swapped = False
         for j in range(0, length - 1):
             if array[j] > array[j + 1]:
@@ -11,6 +11,22 @@ def bubbleSort(array):
     return array
 
 
+def optimizedBubbleSort(array):
+    length = len(array)
+    bound = length - 1
+    for i in range(0, length - 1):
+        newBound = 0
+        for j in range(0, bound):
+            if array[j] > array[j + 1]:
+                array[j + 1], array[j] = array[j], array[j + 1]  # swap the elements
+                newBound = j - 1
+        bound = newBound
+    return array
+
+
 arr = [1, 5, 6, -9, 8, -96, 100, -65, 4, 5, 9]
 sorted_array = bubbleSort(arr)
-print(arr)
+print(sorted_array)
+
+sorted_array2 = optimizedBubbleSort(arr)
+print(sorted_array2)
